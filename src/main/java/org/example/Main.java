bb75d74b63ce1e5c3b0c1f31779a26c4d8fd99c7
Alin2546 <eternalsq1337@gmail.com>
2023-05-01 12:59:25 +0300

    new update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing..");
+
         Scanner cin = new Scanner(System.in);
         int n = cin.nextInt();
         int[] x = new int[n];
 
         for (int i = 0; i < n; i++) {
             x[i] = cin.nextInt();
-
-        }
-
-        for (int i = 0; i < n; i++) {
             System.out.print(x[i] + " ");
 
         }
