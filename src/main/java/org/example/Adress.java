5b8764dbc83f7079aa65ccd0718e4c2e3198f854
Alin2546 <eternalsq1337@gmail.com>
2023-05-06 22:51:09 +0300

    added more classes

---
+++ src/main/java/org/example/Adress.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Adress {
+    private String nameAdress;
+
+    public Adress(String nameAdress) {
+        this.nameAdress = nameAdress;
+    }
+
+    public String getNameAdress() {
+        return nameAdress;
+    }
+
+    public void setNameAdress(String nameAdress) {
+        this.nameAdress = nameAdress;
+    }
+}
--- src/main/java/org/example/Hobby.java
+++ src/main/java/org/example/Hobby.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hobby {
     private String name;
+    private int frequency;
+    public List<Adress> adressList = new ArrayList<>();
 
+    public Hobby(String name, int frequency, List<Adress> adressList) {
+        this.name = name;
+        this.frequency = frequency;
+        this.adressList = adressList;
+    }
 }
